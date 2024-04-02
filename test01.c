adeca37c04ac30b0673ae25a8ca5872e6019d488
RichardENeumann <RichardNeumann@googlemail.com>
2024-04-02 16:21:02 +0200

    Continue tutorial with exportign functions

--- test01.c
+++ test01.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <emscripten.h>
+
+int EMSCRIPTEN_KEEPALIVE fibonacci(int n) {
+  if (n == 0 || n == 1)
+    return 1;
+  else 
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 int main() {
-  printf("Hello Wasmworld!\n");
+  puts("Hello Wasmworld!");
+  int result = fibonacci(5);
+  printf("fibonacci(5) = %d\n", result);
   return 0;
 }
